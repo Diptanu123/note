6cdda9b9dcef9cc4d01eaa15f26b701e3b00c8ee
Diptanu Sarkar <diptanus961@gmail.com>
2024-10-27 12:00:23 +0530

    cors

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
 
@@ -11,7 +11,7 @@ const SearchBar = () => {
 
   const user = useSelector((state) => state.user.userData);
 
-  const username = user.userName;
+  // const username = user.userName;
 
   const handleSearch = async (e) => {
     e.preventDefault();
